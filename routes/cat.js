5b1247c1a4d819cd5e758cbe0ef8e47a9de65a87
cyber2201100146 <2201100146jm@cyber-u.ac.jp>
2025-02-05 04:15:14 +0900

    add cat page

--- routes/cat.js
+++ routes/cat.js
@@ -1,10 +1,19 @@
+var express = require('express');
+var router = express.Router();
 const request = require('request');
-request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
-if (!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-console.log(body);
-const catImageUrl = data[0].url;
-console.log(catImageUrl);
-}
+
+// GET リクエスト時に猫の画像を取得する
+router.get('/', function(req, res, next) {
+  request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      const catImageUrl = data[0].url;  // 画像URLを取得
+      console.log(catImageUrl);  // URLをコンソールに表示
+      res.send(`<img src="${catImageUrl}" alt="Random Cat Image">`);  // 画像をHTMLとして返す
+    } else {
+      res.status(500).send('Error fetching cat image');
+    }
+  });
 });
+
 module.exports = router;
