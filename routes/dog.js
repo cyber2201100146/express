b6c375b2b44bd92d661350e53f6540c5e7fc9316
cyber2201100146 <2201100146jm@cyber-u.ac.jp>
2025-02-05 00:43:11 +0900

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ request('https://dog.ceo/dog-api/', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 console.log(body);
-const catImageUrl = data[0].url;
-console.log(catImageUrl);
+const dogImageUrl = data[0].url;
+console.log(dogImageUrl);
 }
 });
