cd2256f413da49226b8a94ef44539d50d6946cdc
cyber2201100146 <2201100146jm@cyber-u.ac.jp>
2025-02-05 00:03:55 +0900

    add dog page

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
-varexpress= require('express');
-varrouter= express.Router();
+var express = require('express');
+var router = express.Router();
+
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-consturi= "mongodb+srv://2201100146jm:abcd@cluster0.jrl0b.mongodb.net/tests?retryWrites=true&w=majority&appName=Cluster0 ";
-constclient= newMongoClient(uri);
+const uri= "mongodb+srv://2201100146jm:abcd@cluster0.jrl0b.mongodb.net/tests?retryWrites=true&w=majority&appName=Cluster0 ";
+const client= new MongoClient(uri);
 router.get('/', async(req, res) =>{
 // データベース、コレクションを指定
 constdatabase= client.db('notes');
